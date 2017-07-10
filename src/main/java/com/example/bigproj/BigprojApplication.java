34c5e9970c93e9b5b8322e2f6639f6cfc2af3b3b
Shimi Bandiel <shimi@trainologic.com>
2017-07-10 10:26:03 +0300

    step1

--- src/main/java/com/example/bigproj/BigprojApplication.java
+++ src/main/java/com/example/bigproj/BigprojApplication.java
@@ -8,6 +8,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.ConfigurableApplicationContext;
 
 import com.example.bigproj.daos.AccountDao;
+import com.example.bigproj.daos.BigFlow;
 import com.example.bigproj.daos.CustomerDao;
 import com.example.bigproj.model.Account;
 import com.example.bigproj.model.Customer;
@@ -17,28 +18,11 @@ public class BigprojApplication {
 
 	public static void main(String[] args) {
 		BeanFactory bf = SpringApplication.run(BigprojApplication.class, args);
-		CustomerDao cdao = bf.getBean(CustomerDao.class);
+		BigFlow d = bf.getBean(BigFlow.class);
 		AccountDao adao = bf.getBean(AccountDao.class);
-		
-		Customer john = new Customer("John");
-		Customer jane = new Customer("John");
-		
-		
-		cdao.save(Arrays.asList(john, jane));
-		
-		Account a1 = new Account(john, 1000);
-		Account a2 = new Account(john, 2000);
-		Account a3 = new Account(jane, 6000);
-		Account a4 = new Account(jane, 7000);
-		adao.save(Arrays.asList(a1,a2,a3,a4));
-		
-		
-		
-		System.out.println("********************************************");
+		d.insertAll();
+
 		System.out.println(adao.count());
-		
-		
-		
-		
+
 	}
 }
---
+++ src/main/java/com/example/bigproj/daos/BigFlow.java
@@ -0,0 +1,43 @@
+package com.example.bigproj.daos;
+
+import java.util.Arrays;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Repository;
+import org.springframework.transaction.annotation.Transactional;
+
+import com.example.bigproj.model.Account;
+import com.example.bigproj.model.Customer;
+
+@Transactional
+@Repository
+public class BigFlow {
+	private final AccountDao adao;
+	private final CustomerDao cdao;
+
+	@Autowired
+	public BigFlow(AccountDao adao, CustomerDao cdao) {
+		this.adao = adao;
+		this.cdao = cdao;
+	}
+	
+	public void insertAll() {
+		Customer john = new Customer("John");
+		Customer jane = new Customer("John");
+		
+		
+		cdao.save(Arrays.asList(john, jane));
+		
+		Account a1 = new Account(john, 1000);
+		Account a2 = new Account(john, 2000);
+		Account a3 = new Account(jane, 6000);
+		Account a4 = new Account(jane, 7000);
+		adao.save(Arrays.asList(a1,a2,a3,a4));
+		
+	}
+	
+	
+	
+	
+	
+}
